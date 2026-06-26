6de08c1ad1421fc2f710ccaaac09542bcec16146
lushicecream12-tech <lushicecream12@gmail.com>
2026-06-26 19:26:05 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.WZB_FIREBASE_CONFIG = {
+  projectId: "pixel-platoya-1775052774-4138",
+  appId: "1:68236019896:web:wakacje-z-bogiem",
+  databaseURL: "https://pixel-platoya-1775052774-4138-default-rtdb.europe-west1.firebasedatabase.app",
+  storageBucket: "pixel-platoya-1775052774-4138.firebasestorage.app",
+  apiKey: "AIzaSyCBnFU_kmAOHohgaQxDRLZBRm8X8aBeCeg",
+  authDomain: "pixel-platoya-1775052774-4138.firebaseapp.com",
+};
